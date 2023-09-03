b1f44c655e51d30b95bc0d57d625c37ed905841e
Dmytro Truten <dmitriy2503a@gmail.com>
2023-09-03 14:16:06 +0300

    finish navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,21 +10,30 @@ export default function NavBar() {
         <Navbar.Brand>
           <Image src={Logo} />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar__offcanvas" />
-        <Navbar.Offcanvas id="navbar__offcanvas" placement="end">
-          <Offcanvas.Body className="d-flex flex-column flex-lg-row justify-content-between">
+        <Navbar.Toggle aria-controls="navbar-offcanvas" className="p-1" />
+        <Navbar.Offcanvas
+          id="navbar-offcanvas"
+          placement="end"
+          restoreFocus={false}
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>
+              <Image src={Logo} />
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body className="d-flex flex-column flex-lg-row justify-content-between align-items-center">
             <Nav>
-              <Nav.Link>Features</Nav.Link>
-              <Nav.Link>Overview</Nav.Link>
-              <Nav.Link>Store</Nav.Link>
-              <Nav.Link>Blog</Nav.Link>
-              <Nav.Link>Contact</Nav.Link>
+              <Nav.Link className="text-center">Features</Nav.Link>
+              <Nav.Link className="text-center">Overview</Nav.Link>
+              <Nav.Link className="text-center">Store</Nav.Link>
+              <Nav.Link className="text-center">Blog</Nav.Link>
+              <Nav.Link className="text-center">Contact</Nav.Link>
             </Nav>
-            <Container className="offcanvas-body__actions-container d-flex align-items-center px-0 mx-0">
-              <div className="d-flex justify-content-between">
-                <p className="mb-0">Light</p>
+            <Container className="offcanvas-body-actions-container d-flex flex-column-reverse flex-lg-row align-items-center justify-content-between px-0 mx-0 mt-2 mt-lg-0">
+              <div className="d-flex">
+                <p className="switch-theme-text mb-0">Light</p>
                 <span className="toggle-switch mx-3"></span>
-                <p className="mb-0">Dark</p>
+                <p className="switch-theme-text mb-0">Dark</p>
               </div>
               <button className="sign-in-btn d-flex align-items-center justify-content-center">
                 <img className="me-2" src={User} />
