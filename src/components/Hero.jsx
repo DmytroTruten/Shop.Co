e86b2454f3e054276c212de6c844a208ce4c13de
Dmytro Truten <dmitriy2503a@gmail.com>
2023-09-15 21:26:55 +0300

    adjust hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "../styles/Hero/Hero.css";
+import useWindowWidth from "../hooks/useWindowWidth";
 
 export default function Hero() {
+  const width = useWindowWidth();
+
   return (
     <Container fluid className="hero-section-container">
-      <Container>
+      <Container className="px-0">
         <h1 className="hero-title mb-0">
           FIND CLOTHES THAT MATCHES YOUR STYLE
         </h1>
@@ -18,26 +21,44 @@ export default function Hero() {
             Shop Now
           </button>
         </div>
-        <Row className="hero-statistics-row">
-          <Col className="hero-statistics-col" xs={6}>
-            <div className="d-flex flex-column">
+        <Container className="d-flex justify-content-center align-items-center">
+          <Row className="w-100">
+            <Col className="hero-statistics-col d-flex flex-column justify-content-center px-0">
               <h3 className="hero-statistics-number mb-0">200+</h3>
-              <p className="hero-statistics-text mb-0">International Brands</p>
-            </div>
-          </Col>
-          <Col className="hero-statistics-col" xs={6}>
-            <div className="d-flex flex-column">
+              <p className="hero-statistics-text text-nowrap mb-0">
+                International Brands
+              </p>
+            </Col>
+            <Col xs="auto">
+              <span className="hero-statistics-divider d-block"></span>
+            </Col>
+            <Col className="hero-statistics-col d-flex flex-column justify-content-center px-0">
               <h3 className="hero-statistics-number mb-0">2000+</h3>
-              <p className="hero-statistics-text mb-0">High-Quality Products</p>
-            </div>
-          </Col>
-          <Col className="hero-statistics-col d-flex justify-content-center">
-            <div className="d-flex flex-column">
-              <h3 className="hero-statistics-number mb-0">30,000+</h3>
-              <p className="hero-statistics-text mb-0">Happy Customers</p>
-            </div>
-          </Col>
-        </Row>
+              <p className="hero-statistics-text text-nowrap mb-0">
+                High-Quality Products
+              </p>
+            </Col>
+            {width >= 397 && (
+              <Col xs="auto">
+                <span className="hero-statistics-divider d-block"></span>
+              </Col>
+            )}
+            <Col
+              xs={12}
+              sm="auto"
+              className={`hero-statistics-col d-flex flex-column justify-content-center align-items-center ${
+                width < 576 ? "mt-3" : ""
+              } px-0`}
+            >
+              <div className="hero-statistics-inner-col">
+                <h3 className="hero-statistics-number mb-0">30,000+</h3>
+                <p className="hero-statistics-text text-nowrap mb-0">
+                  Happy Customers
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </Container>
     </Container>
   );
